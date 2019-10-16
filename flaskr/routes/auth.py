99bfe103917c34b88b21795a72d00301c5daa93d
Guillaume Rochefort-Mathieu <guillaume.rochefort.mathieu@gmail.com>
2019-10-16 07:05:14 -0400

    Added some comments

--- flaskr/routes/auth.py
+++ flaskr/routes/auth.py
@@ -10,12 +10,24 @@ bp = Blueprint('auth', __name__, url_prefix="/auth")
 @cross_origin(methods=['GET'])
 @login_required
 def logout():
+    """Endpoint use to logout a login user.
+    
+    Returns:
+        (str, int) -- Empty string with 200 if logout was successful, Error otherwise
+    """
+    # Checks whether a user is session
+    # if there is remove it
     if 'user_id' in session:
         session.pop('user_id')
 
+    # If the session is empty
+    # make sure to remove any
+    # data.
     if len(session) <= 0:
         session.clear()
 
+    # If a user is in the global
+    # variable remove it
     if 'user' in g:
         g.pop('user')
 
