0885c95f271706e1b8491def0320b0963fd33d65
Guillaume Rochefort-Mathieu <guillaume.rochefort.mathieu@gmail.com>
2019-10-17 17:58:53 -0400

    Added signature to login endpoint

--- flaskr/routes/auth.py
+++ flaskr/routes/auth.py
@@ -6,6 +6,25 @@ from flaskr.routes.utils import login_required, not_login, cross_origin
 
 bp = Blueprint('auth', __name__, url_prefix="/auth")
 
+@bp.route('/login', methods=[ 'POST', 'OPTIONS' ])
+@cross_origin(methods=[ 'POST' ])
+def login():
+    """Endpoint used to login a user.
+
+    Returns:
+        (dict, int) -- User JSON representation with 200 if login was successful, Error otherwise
+    """
+    
+    # Verify content of the request.json sent by the client
+
+    # If email and password not in query return 400 with {'code':400, 'message': 'Email and password do not match'}
+
+    # If email and password matches add user_id to session
+
+    # Send user.to_json() back to the client with 200
+
+    return '', 200
+
 @bp.route('/logout', methods=['GET', 'OPTIONS'])
 @cross_origin(methods=['GET'])
 @login_required
