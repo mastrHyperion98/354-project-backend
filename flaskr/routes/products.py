d8d09ba066d0476f85d5c8a7cd305a7b3f7838fa
Steven Smith <s_smith1998@outlook.com>
2019-11-02 14:48:18 -0400

    Update routes/products.py

--- flaskr/routes/products.py
+++ flaskr/routes/products.py
@@ -98,45 +98,6 @@ def viewPromotion():
             'message': re.search('DETAIL: (.*)', db_error.args[0]).group(1)
         }, 400
 
-@bp.route("/viewOrder", methods=['GET'])
-def viewOrder():
-
-    try:
-        with session_scope() as db_session:
-
-            queryOrder = db_session.query(Order)
-
-            totalitem =[]
-
-            for item in queryOrder:
-                myitem = {
-                    "id": item.id,
-                    "user_id": item.user_id,
-                    "full_name": item.full_name,
-                    "line1": item.line1,
-                    "line2": item.line2,
-                    "city": item.city,
-                    "country": item.country,
-                    "total_cost": item.total_cost
-                }
-                totalitem.append(myitem)
-            totalitem = {
-                "allitems": totalitem
-            }    
-            return totalitem
-
-        return {
-            'code': 200,
-            'message': 'success'
-        }, 200
-
-    except DBAPIError as db_error:
-        # Returns an error in case of a integrity constraint not being followed.
-        return {
-            'code': 400,
-            'message': re.search('DETAIL: (.*)', db_error.args[0]).group(1)
-        }, 400
-
 
 @bp.route("/getProduct", methods=['POST'])
 def getProduct():
@@ -221,47 +182,6 @@ def getTax():
             )
             # Add to database
             db_session.add(tax)
-
-        return {
-            'code': 200,
-            'message': 'success'
-        }, 200
-
-    except DBAPIError as db_error:
-        # Returns an error in case of a integrity constraint not being followed.
-        return {
-            'code': 400,
-            'message': re.search('DETAIL: (.*)', db_error.args[0]).group(1)
-        }, 400
-
-@bp.route("/getStatus", methods=['POST'])
-def getStatus():
-
-    # Load json data from json schema to variable request.json 'SCHEMA_FOLDER'
-    schemas_direcotry = os.path.join(current_app.root_path, current_app.config['SCHEMA_FOLDER'])
-    schema_filepath = os.path.join(schemas_direcotry, 'getstatus.schema.json')
-    try:
-        with open(schema_filepath) as schema_file:
-            schema = json.loads(schema_file.read())
-            validate(instance=request.json, schema=schema, format_checker=draft7_format_checker)
-
-    except jsonschema.exceptions.ValidationError as validation_error:
-        return {
-            'code': 400,
-            'message': validation_error.message
-        }
-
-    try:
-        with session_scope() as db_session:
-
-            status = request.json.get('status')
-
-            # Create order status object
-            od = Order_Status(
-                status = status
-            )
-            # Add to database
-            db_session.add(od)
 
         return {
             'code': 200,
