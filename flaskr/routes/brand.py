a1cd5d2dcccd95e15c2ca86fccd14a4d0b625b40
Steven Smith <s_smith1998@outlook.com>
2019-11-04 20:52:20 -0500

    update: adds /detBrand -- DELETE method

--- flaskr/routes/brand.py
+++ flaskr/routes/brand.py
@@ -74,6 +74,7 @@ def viewBrand():
 
             for item in queryProduct:
                 myitem = {
+                    "id": item.id,
                     "name": item.name,
                     "description": item.description,
                     "logo": item.logo
@@ -95,3 +96,39 @@ def viewBrand():
             'code': 400,
             'message': re.search('DETAIL: (.*)', db_error.args[0]).group(1)
         }, 400
+
+@bp.route("/delBrand", methods=["DELETE"])
+def delBrand():
+    # Load json data from json schema to variable request.json 'SCHEMA_FOLDER'
+    schemas_direcotry = os.path.join(current_app.root_path, current_app.config['SCHEMA_FOLDER'])
+    schema_filepath = os.path.join(schemas_direcotry, 'delbrand.schema.json')
+    try:
+        with open(schema_filepath) as schema_file:
+            schema = json.loads(schema_file.read())
+            validate(instance=request.json, schema=schema, format_checker=draft7_format_checker)
+
+    except jsonschema.exceptions.ValidationError as validation_error:
+        return {
+            'code': 400,
+            'message': validation_error.message
+        }
+
+    try:
+
+        with session_scope() as db_session:
+
+            queryProduct = db_session.query(Brand).filter(Brand.id == request.json.get("id")).one()
+
+            db_session.delete(queryProduct)
+
+            return {
+                       'code': 200,
+                       'message': 'success'
+                   }, 200
+
+    except DBAPIError as db_error:
+        # Returns an error in case of a integrity constraint not being followed.
+        return {
+                   'code': 400,
+                   'message': re.search('DETAIL: (.*)', db_error.args[0]).group(1)
+               }, 400
