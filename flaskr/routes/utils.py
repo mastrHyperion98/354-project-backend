79a272abda65601ec97d49efb48a9f649212c352
Steven <s_smith1998@outlook.com>
2019-11-23 23:42:36 -0500

    Fix admin_required decorator

--- flaskr/routes/utils.py
+++ flaskr/routes/utils.py
@@ -57,11 +57,10 @@ def is_logged_in():
 def admin_required(func):
     @wraps(func)
     def decorated_function(*args, **kwargs):
-        if 'user' not in g:
-            if not g.user.is_admin:
-                return {
+        if not g.user.is_admin:
+            return {
                     'code': 400,
                     'message': 'Unauthorized Access'
                 }
-            return func(*args, **kwargs)
+        return func(*args, **kwargs)
     return decorated_function
