ed9240be9349092c4a7ca636806ddc3eb375dead
khanhngd24 <32332385+khanhngd24@users.noreply.github.com>
2019-11-27 16:06:25 -0500

    Add files via upload

--- flaskr/routes/products.py
+++ flaskr/routes/products.py
@@ -17,13 +17,68 @@ from sqlalchemy import or_
 from flaskr.db import session_scope
 from flaskr.models.Product import Product
 from flaskr.models.Price import Price
-from flaskr.models.Brand import Brand
-from flaskr.models.Category import Category
-from flaskr.models.Section import Section
-from flaskr.models.Review import review
 from flaskr.routes.utils import login_required, not_login, cross_origin
+from flaskr.models.Category import Category
 
 bp = Blueprint('products', __name__, url_prefix='/products')
+
+@bp.route('', methods=['GET', 'OPTIONS'])
+@cross_origin(methods=['GET'])
+def getProducts():
+    # Validate that only the valid Query properties from the JSON schema filter_product.schema.json
+    schemas_direcotry = os.path.join(current_app.root_path, current_app.config['SCHEMA_FOLDER'])
+    schema_filepath = os.path.join(schemas_direcotry, 'filter_product.schema.json')
+    try:
+        with open(schema_filepath) as schema_file:
+            schema = json.loads(schema_file.read())
+            validate(instance=request.args, schema=schema, format_checker=draft7_format_checker)
+    except jsonschema.exceptions.ValidationError as validation_error:
+        return {
+            'code': 400,
+            'message': validation_error.message
+        }, 400
+
+    with session_scope() as db_session:
+        products = db_session.query(Product)
+        count = None
+        if 'category' in request.args:
+            category = db_session.query(Category).filter(Category.permalink == request.args['category']).first()
+            if category is None:
+                return {
+                    'code': 404,
+                    'message': 'Category not found'
+                }, 404
+
+            count = category.products.count()
+
+            products = category.products
+
+        if 'q' in request.args:
+            tokens = request.args['q'].strip().split()
+            or_instruction = []
+            
+            for token in tokens:
+                or_instruction.append(Product.name.match(token))
+                or_instruction.append(Product.description.match(token))
+
+            products = db_session.query(Product).filter(or_(*or_instruction))
+
+            count = products.count()
+
+        if 'priceOrderFilter' in request.args:
+            priceOrder = request.args['priceOrderFilter']
+
+            if priceOrder == 'lowToHigh':
+                products.order_by(Product.price.first().asc())
+            elif priceOrder == 'highToLow':
+                products.order_by(Product.price.first().desc())
+        
+        if count is None:
+            count = products.count()
+
+        products = products.limit(request.args['limit']).offset(int(request.args['limit'])*int(request.args['page']))
+
+        return {'products':[ product.to_json() for product in products.all()], 'count': count}
 
 @bp.route('', methods=['POST', 'OPTIONS'])
 @cross_origin(methods=['GET', 'POST', 'HEAD'])
@@ -39,6 +94,7 @@ def createProduct():
     # Validate that only the valid Product properties from the JSON schema new_product.schema.json
     schemas_direcotry = os.path.join(current_app.root_path, current_app.config['SCHEMA_FOLDER'])
     schema_filepath = os.path.join(schemas_direcotry, 'new_product.schema.json')
+
     try:
         with open(schema_filepath) as schema_file:
             schema = json.loads(schema_file.read())
@@ -66,222 +122,13 @@ def createProduct():
 
             # Adds the price to the product
             new_product.price.append(Price(amount=request.json['price']))
-            
+
             db_session.add(new_product)
 
             # Commit new product to database making sure of the integrity of the relations.
             db_session.commit()
 
             return new_product.to_json(), 200
-    except DBAPIError as db_error:
-        # Returns an error in case of a integrity constraint not being followed.
-        return {
-            'code': 400,
-            'message': re.search('DETAIL: (.*)', db_error.args[0]).group(1)
-        }, 400
-    
-@bp.route("/<int:product_id>/addreview", methods =["POST"])
-@login_required
-def save(product_id):
-
-    # Load json data from json schema to variable user_info.json 'SCHEMA_FOLDER'
-    schemas_direcotry = os.path.join(current_app.root_path, current_app.config['SCHEMA_FOLDER'])
-    schema_filepath = os.path.join(schemas_direcotry, 'review.schema.json')
-    try:
-        with open(schema_filepath) as schema_file:
-            schema = json.loads(schema_file.read())
-            validate(instance=request.json, schema=schema, format_checker=draft7_format_checker)
-    except jsonschema.exceptions.ValidationError as validation_error:
-        return {
-            'code': 400,
-            'message': validation_error.message
-        }
-
-    try:
-        # Check if cart id exists with cart items
-        with session_scope() as db_session:
-
-            # get user_id from json
-            #user_id = request.json.get("user_id")
-            user_id = session['user_id']
-            #product_id = request.json.get("product_id")
-
-            comment = request.json.get("comment")
-            score = request.json.get("score")
-
-            # check if user has bought this product id
-            queryOrder = db_session.query(Order).filter(Order.user_id == user_id)
-            count = 0
-            flag=0
-            for item in queryOrder:
-                
-                mydate=item.date
-                now=date.today()
-                diff = int((now - mydate).days)
-                if diff >= 15:
-                    flag = 1
-                queryOrderLine = db_session.query(OrderLine).filter(OrderLine.order_id == item.id)
-                for lineitem in queryOrderLine:
-                    if lineitem.product_id == product_id:
-                        count = count + 1
-            if flag==0:
-                return {
-                        "code": 400,
-                        "message": "You have to use the product for 15 days before you can post review"
-                    }, 400
-            
-            word = comment.split(" ")
-            if len(word) > 400:
-                return {
-                        "code": 400,
-                        "message": "Comment has to be less than 400 words"
-                    }, 400
-            
-            if count > 0:
-                if score <= 5 and score >= 0:
-                    myreview = review(
-                            user_id = user_id,
-                            #user_id = session['user_id'],
-                            product_id = product_id,
-                            comment = comment,
-                            score = score
-                            )
-                    db_session.add(myreview)
-                    #db_session.flush()
-
-                    return {
-                        "code": 200,
-                        "message": myreview.to_json()
-                    }, 200
-                else:
-                    return {
-                        "code": 400,
-                        "message": "Score has to be in range 0 to 5"
-                    }, 400
-            else:
-                return {
-                    "code": 400,
-                    "message": "This user can't post review because he hasn't bought any of these products"
-                }, 400
-
-    except DBAPIError as db_error:
-        # Returns an error in case of a integrity constraint not being followed.
-        return {
-            'code': 400,
-            'message': 'error: ' + db_error.args[0]
-        }, 400
-
-
-
-@bp.route("/<int:product_id>/viewreview", methods =["GET"])
-@login_required
-def view(product_id):
-
-    try:
-        # Check if cart id exists with cart items
-        with session_scope() as db_session:
-
-            myreview = db_session.query(review)
-
-            array=[]
-            for item in myreview:
-                if item.product_id == product_id:
-                    array.append(item.to_json())
-
-            return {
-                "code": 200,
-                "message": array
-            }, 200
-
-    except DBAPIError as db_error:
-        # Returns an error in case of a integrity constraint not being followed.
-        return {
-            'code': 400,
-            'message': 'error: ' + db_error.args[0]
-        }, 400
-
-@bp.route("/search", methods=['GET'])
-def searchProduct():
-
-    # Load json data from json schema to variable request.json 'SCHEMA_FOLDER'
-    schemas_direcotry = os.path.join(current_app.root_path, current_app.config['SCHEMA_FOLDER'])
-    schema_filepath = os.path.join(schemas_direcotry, 'search.schema.json')
-    try:
-        with open(schema_filepath) as schema_file:
-            schema = json.loads(schema_file.read())
-            validate(instance=request.json, schema=schema, format_checker=draft7_format_checker)
-
-    except jsonschema.exceptions.ValidationError as validation_error:
-        return {
-            'code': 400,
-            'message': validation_error.message
-        }
-    
-    try:
-        with session_scope() as db_session:
-            search = request.json.get("search")
-            queryProduct = db_session.query(Product)
-            array=[]
-            for item in queryProduct:
-                flag = 0 
-                if search in item.name or search in item.description or search in item.permalink:
-                    array.append(item.to_json())
-                    flag = 1
-                if flag == 0:
-                    queryBrand = db_session.query(Brand).filter(Brand.id == item.brand_id).one()
-                    if search in queryBrand.description or search in queryBrand.logo or search in queryBrand.name:
-                        array.append(item.to_json())
-                        flag = 1
-                    if flag == 0:
-                        queryCategory = db_session.query(Category).filter(Category.id == item.category_id).one()
-                        if search in queryCategory.name or search in queryCategory.description or search in queryCategory.permalink:
-                            array.append(item.to_json())
-                            flag = 1
-                        if flag == 0:
-                            querySection = db_session.query(Section).filter(Section.id == queryCategory.section_id).one()
-                            if search in querySection.name or search in querySection.description or search in querySection.permalink:
-                                array.append(item.to_json())
-
-            if len(array) > 0:
-                return {
-                    "code": 200,
-                    "result": array
-                }, 200
-            else:
-                return {
-                    "code": 400,
-                    "message": "not found"
-                }, 400
-
-    except DBAPIError as db_error:
-        # Returns an error in case of a integrity constraint not being followed.
-        return {
-            'code': 400,
-            'message': re.search('DETAIL: (.*)', db_error.args[0]).group(1)
-        }, 400
-
-@bp.route("/view", methods=['GET'])
-def viewProduct():
-
-    try:
-        with session_scope() as db_session:
-
-            queryProduct = db_session.query(Product)
-
-            totalitem =[]
-
-            for item in queryProduct:
-                totalitem.append(item.to_json())
-            totalitem = {
-                "allitems": totalitem
-            }    
-            return totalitem
-
-        return {
-            'code': 200,
-            'message': 'success'
-        }, 200
-
     except DBAPIError as db_error:
         # Returns an error in case of a integrity constraint not being followed.
         return {
--- flaskr/routes/users.py
+++ flaskr/routes/users.py
@@ -15,109 +15,10 @@ from sqlalchemy import or_
 from flaskr.db import session_scope
 from flaskr.email import send
 from flaskr.models.User import User
+from flaskr.models.Cart import Cart
 from flaskr.routes.utils import login_required, not_login, cross_origin
 
 bp = Blueprint('users', __name__, url_prefix='/users')
-
-@bp.route("/add/<int:rev_id>", methods =["POST"])
-@login_required
-def save(rev_id):
-
-    # Load json data from json schema to variable user_info.json 'SCHEMA_FOLDER'
-    schemas_direcotry = os.path.join(current_app.root_path, current_app.config['SCHEMA_FOLDER'])
-    schema_filepath = os.path.join(schemas_direcotry, 'review.schema.json')
-    try:
-        with open(schema_filepath) as schema_file:
-            schema = json.loads(schema_file.read())
-            validate(instance=request.json, schema=schema, format_checker=draft7_format_checker)
-    except jsonschema.exceptions.ValidationError as validation_error:
-        return {
-            'code': 400,
-            'message': validation_error.message
-        }
-
-    try:
-        # Check if cart id exists with cart items
-        with session_scope() as db_session:
-
-            # get user_id from json
-            #user_id = request.json.get("user_id")
-            user_id = rev_id
-            product_id = request.json.get("product_id")
-            comment = request.json.get("comment")
-            score = request.json.get("score")
-
-            # check if user has bought this product id
-            queryOrder = db_session.query(Order).filter(Order.user_id == user_id)
-            count = 0
-            for item in queryOrder:
-                queryOrderLine = db_session.query(OrderLine).filter(OrderLine.order_id == item.id)
-                for lineitem in queryOrderLine:
-                    if lineitem.product_id == product_id:
-                        count = count + 1
-
-            if count > 0:
-                if score <= 5 and score >= 0:
-                    myreview = review(
-                            user_id = user_id,
-                            #user_id = session['user_id'],
-                            product_id = product_id,
-                            comment = comment,
-                            score = score
-                            )
-                    db_session.add(myreview)
-                    #db_session.flush()
-
-                    return {
-                        "code": 200,
-                        "message": myreview.to_json()
-                    }, 200
-                else:
-                    return {
-                        "code": 400,
-                        "message": "Score has to be in range 1 to 5"
-                    }, 400
-            else:
-                return {
-                    "code": 400,
-                    "message": "This user can't post review because he hasn't bought any of these products"
-                }, 400
-
-    except DBAPIError as db_error:
-        # Returns an error in case of a integrity constraint not being followed.
-        return {
-            'code': 400,
-            'message': 'error: ' + db_error.args[0]
-        }, 400
-
-
-
-@bp.route("/view/<int:rev_id>", methods =["GET"])
-@login_required
-def view(rev_id):
-
-    try:
-        # Check if cart id exists with cart items
-        with session_scope() as db_session:
-
-            myreview = db_session.query(review)
-
-            array=[]
-            for item in myreview:
-                if item.user_id == rev_id:
-                    array.append(item.to_json())
-
-            return {
-                "code": 200,
-                "message": array
-            }, 200
-
-    except DBAPIError as db_error:
-        # Returns an error in case of a integrity constraint not being followed.
-        return {
-            'code': 400,
-            'message': 'error: ' + db_error.args[0]
-        }, 400
 
 @bp.route('', methods=[ 'GET', 'HEAD', 'OPTIONS' ])
 @cross_origin(methods=[ 'GET', 'POST', 'HEAD' ])
@@ -187,6 +88,7 @@ def registerUser():
     try:
         with session_scope() as db_session:
             new_user = User(first_name=request.json['firstName'], last_name=request.json['lastName'], username=request.json['username'], email=request.json['email'], password=argon2.hash(request.json['password']))
+            new_user.reset_password = False
             db_session.add(new_user)
 
             # Commit new user to database making sure of the integrity of the relations.
@@ -197,7 +99,7 @@ def registerUser():
 
             # TODO Send confirmation email, for now only sending welcoming email.
             send(current_app.config['SMTP_USERNAME'], new_user.email, "Welcome to 354TheStars!", "<html><body><p>Welcome to 354TheStars!</p></body></html>" ,"Welcome to 354TheStars!")
-
+            new_user.cart = Cart(user_id=new_user.id)
             return new_user.to_json(), 200
     except DBAPIError as db_error:
 
@@ -264,6 +166,7 @@ def updateSelf():
                 # if k == password hash password
                 if k == "password":
                     user.__dict__[k] = argon2.hash(v)
+                    user.reset_password = False
                 else:
                     user.__dict__[k] = v
 
