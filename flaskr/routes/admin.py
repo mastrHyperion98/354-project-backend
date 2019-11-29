1b686701d3863c9c6452f8222c9f795889fbe01d
Steven <s_smith1998@outlook.com>
2019-11-29 00:18:08 -0500

    improve changes to view seller leaderboard on 1 date, a range of date or all

--- flaskr/routes/admin.py
+++ flaskr/routes/admin.py
@@ -26,7 +26,7 @@ from flaskr.models.SellerRecord import SellerRecord
 
 bp = Blueprint('admin', __name__, url_prefix='/admin')
 
-@bp.route("/sales", methods=['GET', 'OPTIONS'])
+@bp.route('/sales', methods=['GET', 'OPTIONS'])
 @cross_origin(methods=['GET'])
 @login_required
 @admin_required
@@ -68,8 +68,8 @@ def view_total_sales():
         'numberItems': nmbr_itm
            }, 200
 
-@bp.route("/sales/<string:start_date>", methods=['GET', 'OPTIONS'])
-@bp.route("/sales/<string:start_date>/<string:end_date>", methods=['GET', 'OPTIONS'])
+@bp.route('/sales/<string:start_date>', methods=['GET', 'OPTIONS'])
+@bp.route('/sales/<string:start_date>/<string:end_date>', methods=['GET', 'OPTIONS'])
 @cross_origin(methods=['GET'])
 @login_required
 @admin_required
@@ -121,7 +121,7 @@ def view_total_sales_by_date(start_date, end_date= None):
         'numberItems': nmbr_itm
            }, 200
 
-@bp.route("/update/<string:username>", methods=['PATCH', 'OPTIONS'])
+@bp.route('/update/<string:username>', methods=['PATCH', 'OPTIONS'])
 @cross_origin(methods=['PATCH', 'GET'])
 @login_required
 @admin_required
@@ -167,7 +167,7 @@ def admin_update_user(username):
             'message': re.search('DETAIL: (.*)', db_error.args[0]).group(1)
         }, 400
 
-@bp.route("/sales/leaderboard", methods=['GET', 'OPTIONS'])
+@bp.route('/sales/leaderboard', methods=['GET', 'OPTIONS'])
 @cross_origin(methods=['GET'])
 @login_required
 @admin_required
@@ -189,6 +189,67 @@ def view_sales_leaderboard():
                     products = db_session.query(Product).filter(Product.user_id == user.id).all()
                     for product in products:
                         order_lines = db_session.query(OrderLine).filter(OrderLine.product_id == product.id)
+                        for order_line in order_lines:
+                            sales = sales + order_line.quantity
+                    seller = SellerRecord(username, sales)
+                    leaderboard.append(seller)
+                #Sort the entries
+                leaderboard.sort()
+                first_ten = []
+                for i in range(min(10,len(leaderboard))):
+                    first_ten.append(leaderboard[i].to_json())
+
+        except DBAPIError as db_error:
+            # Returns an error in case of a integrity constraint not being followed.
+            return {
+                       'code': 400,
+                       'message': re.search('DETAIL: (.*)', db_error.args[0]).group(1)
+                   }, 400
+        except NoResultFound:
+            # Returns an error in case of a integrity constraint not being followed.
+            return {
+                       'code': 400,
+                       'message': "No sales have been registered"
+                   }, 400
+        return{
+            "top_sellers": first_ten
+              }, 200
+
+@bp.route('/sales/leaderboard/<string:start_date>', methods=['GET', 'OPTIONS'])
+@bp.route('/sales/leaderboard/<string:start_date>/<string:end_date>', methods=['GET', 'OPTIONS'])
+@cross_origin(methods=['GET'])
+@login_required
+@admin_required
+def view_sales_leaderboard_by_date(start_date, end_date= None):
+        # Later will add the ability to sort by date and Category
+        """Endpoint use to compute the total number of items sold between two dates.
+
+             Returns:
+                 (str, int) -- Returns a string with the number of sales.
+             """
+        try:
+            with session_scope() as db_session:
+                # Added filters by date
+                users = db_session.query(User).all()
+                leaderboard = []
+                for user in users:
+                    username = user.username
+                    sales = 0
+                    products = db_session.query(Product).filter(Product.user_id == user.id).all()
+                    for product in products:
+                        if end_date is not None:
+                            if validate(start_date) and validate(end_date):
+                                pass
+                            else:
+                                return '', 404
+                            order_lines = db_session.query(OrderLine).filter(OrderLine.product_id == product.id, OrderLine.date_fulfilled.between(start_date,end_date))
+                        else:
+                            if validate(start_date):
+                                pass
+                            else:
+                                return '', 404
+                            order_lines = db_session.query(OrderLine).filter(OrderLine.product_id == product.id, OrderLine.date_fulfilled == start_date)
+
                         for order_line in order_lines:
                             sales = sales + order_line.quantity
                     seller = SellerRecord(username, sales)
