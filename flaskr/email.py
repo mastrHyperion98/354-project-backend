ae6cf36961d357bf5e9c33a0b266de43f8937fbb
CommanderMark <markjarjour@gmail.com>
2019-12-01 13:06:51 -0500

    no message

--- flaskr/email.py
+++ flaskr/email.py
@@ -5,7 +5,7 @@ from email.mime.multipart import MIMEMultipart
 
 def send(sender_email, receiver_email, subject, html_message, plain_text_message):
     """Sends an email from the sender emal to the receiver email.
-    
+
     Arguments:
         sender_email {string} -- Email that is sending this message
         receiver_email {string} -- Email that is receiving the message
@@ -13,12 +13,13 @@ def send(sender_email, receiver_email, subject, html_message, plain_text_message
         html_message {string} -- Message formatted in a HTML format
         plain_text_message {string} -- Message formatter in plain text
     """
+    return
     context = ssl.create_default_context()
     with smtplib.SMTP(current_app.config["SMTP_HOST"], current_app.config["SMTP_PORT"]) as server:
-        
-        # Note that the connection is not secured but the paylods being exchanged 
+
+        # Note that the connection is not secured but the paylods being exchanged
         # is being encrypted
-        
+
         # Key exchange for payload encryption
         server.ehlo()
         server.starttls(context=context)
@@ -26,7 +27,7 @@ def send(sender_email, receiver_email, subject, html_message, plain_text_message
 
         # Login
         server.login(current_app.config["SMTP_LOGIN"], current_app.config["SMTP_PASSWORD"])
-        
+
         # Construct messsage
         message = MIMEMultipart("alternative")
         message["Subject"] = subject
